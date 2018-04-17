12a93a63dc52d15cfa6bc45e27e34abf18399670
gollySun <15621101300@163.com>
2018-04-17 18:11:49 +0800

    创建一个框架项目

---
+++ golly_Structure/golly_Structure/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  golly_Structure
+//
+//  Created by 孙国立 on 2018/4/17.
+//  Copyright © 2018年 孙国立. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
